130bed276a0f8c0afa4d1a774258272fa6c9e775
Santiago <sig20p6@gmail.com>
2025-03-26 02:03:56 -0300

    Primer commit de mi portafolio web

---
+++ java.js
@@ -0,0 +1,5 @@
+// Validación y acción del formulario de contacto
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por tu mensaje!');
+});
